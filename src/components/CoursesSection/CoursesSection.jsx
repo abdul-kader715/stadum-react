8488da632ddcb9213a37ae326ffd1c24326422ab
abdul-kader715 <limonislam715@gmail.com>
2025-04-29 01:02:02 +0600

    courses

--- src/components/CoursesSection/CoursesSection.jsx
+++ src/components/CoursesSection/CoursesSection.jsx
@@ -98,23 +98,200 @@ const CoursesSection = (props) => {
                                        </Link>
                                     </div>
                                  </div>
-
                               </div>
-
-
-                              {/* <div className="project_overlay">
-                                 
-                                    
-                                 </Link>
-                              </div> */}
                            </div>
                         </div>
                      ))}
                   </div>
                }
-               {activeTab === "tab2" && <div>Content for Tab 2</div>}
-               {activeTab === "tab3" && <div>Content for Tab 3</div>}
+               {activeTab === "tab2" &&
+                  <div className="row courses_content">
+                     {CoursesData.slice(0, 5).map((courses, pitem) => (
+                        <div className="col col-lg-4  col-md-6 col-12" key={pitem}>
+                           <div className="courses_item">
+                              <div className="image">
+                                 <img src={courses.img} alt="" />
+                                 <button className='wish'>
+                                    <img src={wish} alt="" />
+                                 </button>
+                              </div>
+                              <div className="content">
+                                 <span>{courses.subjet}</span>
+                                 <h2>
+                                    <Link onClick={ClickHandler} to={`/courses-single/${courses.slug}`}>
+                                       {courses.title}
+                                    </Link>
+                                 </h2>
+                                 <div className="reting">
+                                    <ul>
+                                       {[...Array(5)].map((_, i) => (
+                                          <li key={i}><img src={star} alt="" /></li>
+                                       ))}
+                                       <li><span>4.9(10k)</span></li>
+                                    </ul>
+                                 </div>
+                                 <div className="features">
+                                    <ul>
+                                       <li><img src={SemImg} alt="" />{courses.semesters} Semesters</li>
+                                       <li><img src={Years} alt="" />{courses.year}  Years</li>
+                                       <li><img src={Seats} alt="" />{courses.seats} Seats</li>
+                                    </ul>
+                                 </div>
+                                 <div className="actions">
+                                    <div className="ath">
+                                       <div className="image">
+                                          <img src={courses.athImg} alt="" />
+                                       </div>
+                                       <h3>By:{courses.ath}</h3>
+                                    </div>
+                                    <div className="athbtn">
+                                       <Link to='/'>Enroll Now
+                                          <div className="arrow">
+                                             <svg width="8" height="16" viewBox="0 0 8 16" fill="none">
+                                                <path d="M0.666701 16C0.496203 16 0.325538 15.9289 0.195373 15.7869C-0.0651243 15.5027 -0.0651243 15.0425 0.195373 14.7585L6.39064 7.99999L0.195373 1.24143C-0.0651243 0.957251 -0.0651243 0.497069 0.195373 0.213068C0.45587 -0.0709319 0.877699 -0.0711137 1.13803 0.213068L7.80463 7.48581C8.06512 7.76999 8.06512 8.23017 7.80463 8.51417L1.13803 15.7869C1.00786 15.9289 0.837199 16 0.666701 16Z" fill="#03060E" />
+                                             </svg>
+                                          </div>
+                                          <div className="hover-arrow">
+                                             <svg width="16" height="14" viewBox="0 0 16 14" fill="none">
+                                                <path d="M8.00003 0.414429C8.00003 1.11063 8.68997 2.15243 9.38743 3.02736C10.2856 4.15383 11.3574 5.1379 12.5874 5.88923C13.5088 6.45183 14.6276 6.99189 15.5264 6.99189M8.00003 13.5856C8.00003 12.8894 8.68997 11.8476 9.38743 10.9727C10.2856 9.84629 11.3574 8.86216 12.5874 8.11083C13.5088 7.54823 14.6276 7.00816 15.5264 7.00816M15.5264 7.00003H0.473633" stroke="white" />
+                                             </svg>
+                                          </div>
 
+                                       </Link>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     ))}
+                  </div>
+               }
+               {activeTab === "tab3" &&
+                  <div className="row courses_content">
+                     {CoursesData.slice(0, 3).map((courses, pitem) => (
+                        <div className="col col-lg-4  col-md-6 col-12" key={pitem}>
+                           <div className="courses_item">
+                              <div className="image">
+                                 <img src={courses.img} alt="" />
+                                 <button className='wish'>
+                                    <img src={wish} alt="" />
+                                 </button>
+                              </div>
+                              <div className="content">
+                                 <span>{courses.subjet}</span>
+                                 <h2>
+                                    <Link onClick={ClickHandler} to={`/courses-single/${courses.slug}`}>
+                                       {courses.title}
+                                    </Link>
+                                 </h2>
+                                 <div className="reting">
+                                    <ul>
+                                       {[...Array(5)].map((_, i) => (
+                                          <li key={i}><img src={star} alt="" /></li>
+                                       ))}
+                                       <li><span>4.9(10k)</span></li>
+                                    </ul>
+                                 </div>
+                                 <div className="features">
+                                    <ul>
+                                       <li><img src={SemImg} alt="" />{courses.semesters} Semesters</li>
+                                       <li><img src={Years} alt="" />{courses.year}  Years</li>
+                                       <li><img src={Seats} alt="" />{courses.seats} Seats</li>
+                                    </ul>
+                                 </div>
+                                 <div className="actions">
+                                    <div className="ath">
+                                       <div className="image">
+                                          <img src={courses.athImg} alt="" />
+                                       </div>
+                                       <h3>By:{courses.ath}</h3>
+                                    </div>
+                                    <div className="athbtn">
+                                       <Link to='/'>Enroll Now
+                                          <div className="arrow">
+                                             <svg width="8" height="16" viewBox="0 0 8 16" fill="none">
+                                                <path d="M0.666701 16C0.496203 16 0.325538 15.9289 0.195373 15.7869C-0.0651243 15.5027 -0.0651243 15.0425 0.195373 14.7585L6.39064 7.99999L0.195373 1.24143C-0.0651243 0.957251 -0.0651243 0.497069 0.195373 0.213068C0.45587 -0.0709319 0.877699 -0.0711137 1.13803 0.213068L7.80463 7.48581C8.06512 7.76999 8.06512 8.23017 7.80463 8.51417L1.13803 15.7869C1.00786 15.9289 0.837199 16 0.666701 16Z" fill="#03060E" />
+                                             </svg>
+                                          </div>
+                                          <div className="hover-arrow">
+                                             <svg width="16" height="14" viewBox="0 0 16 14" fill="none">
+                                                <path d="M8.00003 0.414429C8.00003 1.11063 8.68997 2.15243 9.38743 3.02736C10.2856 4.15383 11.3574 5.1379 12.5874 5.88923C13.5088 6.45183 14.6276 6.99189 15.5264 6.99189M8.00003 13.5856C8.00003 12.8894 8.68997 11.8476 9.38743 10.9727C10.2856 9.84629 11.3574 8.86216 12.5874 8.11083C13.5088 7.54823 14.6276 7.00816 15.5264 7.00816M15.5264 7.00003H0.473633" stroke="white" />
+                                             </svg>
+                                          </div>
+
+                                       </Link>
+                                    </div>
+                                 </div>
+
+                              </div>
+                           </div>
+                        </div>
+                     ))}
+                  </div>
+               }
+               {activeTab === "tab4" &&
+                  <div className="row courses_content">
+                     {CoursesData.slice(0, 2).map((courses, pitem) => (
+                        <div className="col col-lg-4  col-md-6 col-12" key={pitem}>
+                           <div className="courses_item">
+                              <div className="image">
+                                 <img src={courses.img} alt="" />
+                                 <button className='wish'>
+                                    <img src={wish} alt="" />
+                                 </button>
+                              </div>
+                              <div className="content">
+                                 <span>{courses.subjet}</span>
+                                 <h2>
+                                    <Link onClick={ClickHandler} to={`/courses-single/${courses.slug}`}>
+                                       {courses.title}
+                                    </Link>
+                                 </h2>
+                                 <div className="reting">
+                                    <ul>
+                                       {[...Array(5)].map((_, i) => (
+                                          <li key={i}><img src={star} alt="" /></li>
+                                       ))}
+                                       <li><span>4.9(10k)</span></li>
+                                    </ul>
+                                 </div>
+                                 <div className="features">
+                                    <ul>
+                                       <li><img src={SemImg} alt="" />{courses.semesters} Semesters</li>
+                                       <li><img src={Years} alt="" />{courses.year}  Years</li>
+                                       <li><img src={Seats} alt="" />{courses.seats} Seats</li>
+                                    </ul>
+                                 </div>
+                                 <div className="actions">
+                                    <div className="ath">
+                                       <div className="image">
+                                          <img src={courses.athImg} alt="" />
+                                       </div>
+                                       <h3>By:{courses.ath}</h3>
+                                    </div>
+                                    <div className="athbtn">
+                                       <Link to='/'>Enroll Now
+                                          <div className="arrow">
+                                             <svg width="8" height="16" viewBox="0 0 8 16" fill="none">
+                                                <path d="M0.666701 16C0.496203 16 0.325538 15.9289 0.195373 15.7869C-0.0651243 15.5027 -0.0651243 15.0425 0.195373 14.7585L6.39064 7.99999L0.195373 1.24143C-0.0651243 0.957251 -0.0651243 0.497069 0.195373 0.213068C0.45587 -0.0709319 0.877699 -0.0711137 1.13803 0.213068L7.80463 7.48581C8.06512 7.76999 8.06512 8.23017 7.80463 8.51417L1.13803 15.7869C1.00786 15.9289 0.837199 16 0.666701 16Z" fill="#03060E" />
+                                             </svg>
+                                          </div>
+                                          <div className="hover-arrow">
+                                             <svg width="16" height="14" viewBox="0 0 16 14" fill="none">
+                                                <path d="M8.00003 0.414429C8.00003 1.11063 8.68997 2.15243 9.38743 3.02736C10.2856 4.15383 11.3574 5.1379 12.5874 5.88923C13.5088 6.45183 14.6276 6.99189 15.5264 6.99189M8.00003 13.5856C8.00003 12.8894 8.68997 11.8476 9.38743 10.9727C10.2856 9.84629 11.3574 8.86216 12.5874 8.11083C13.5088 7.54823 14.6276 7.00816 15.5264 7.00816M15.5264 7.00003H0.473633" stroke="white" />
+                                             </svg>
+                                          </div>
+
+                                       </Link>
+                                    </div>
+                                 </div>
+
+                              </div>
+                           </div>
+                        </div>
+                     ))}
+                  </div>
+               }
             </div>
             <div className="courses_btn">
                <Link to='/' className="btn_primary" onClick={ClickHandler}>
